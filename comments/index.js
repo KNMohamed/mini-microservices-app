fe322587b6e815b3ce293af94c82cf19ce72bedd
knmohamed <khalid.n.mohamed@outlook.com>
2024-05-05 18:44:38 -0400

    Send post and comment events to eventbus

--- comments/index.js
+++ comments/index.js
@@ -2,18 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const commentsByPostId = {};
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async(req, res) => {
     const id = randomBytes(4).toString('hex');
     const { content } = req.body;
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({ id, content });
     commentsByPostId[req.params.id] = comments;
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id,
+        content,
+        postId: req.params.id
+      }
+    });
     res.status(201).send(comments[comments.length - 1]);
 });
 
