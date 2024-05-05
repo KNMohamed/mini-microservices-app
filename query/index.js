f19da56863b71cc3633f3c6b2fb0a0645f06fc16
knmohamed <khalid.n.mohamed@outlook.com>
2024-05-05 18:58:59 -0400

    Create query service

---
+++ query/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+const handleEvent = (type,data) => {
+  switch(type){
+    case "PostCreated": {
+      const {id,title} = data;
+      posts[id] = {id,title, comments: []};
+      break;
+    }
+    case "CommentCreated": {
+      const {id ,postId, content, status} = data;
+      posts[postId].comments.push({id,content, status});
+      break;
+    }
+    case "CommentUpdated": {
+      const {id,content,postId, status} = data;
+      const post = posts[postId];
+      const comment = post.comments.find(comment => {
+        return comment.id === id;
+      });
+      comment.content = content;
+      comment.status = status;
+      console.log(comment);
+      break;
+    }
+  }
+}
+
+app.get('/posts', (req,res)=>{
+  res.send(posts);
+});
+
+app.post('/events', (req,res)=>{
+  const {type, data} = req.body;
+  handleEvent(type, data);
+  console.log("Recieved event: ", req.body.type);
+  res.send({status: "OK"});
+});
+
+app.listen(4002, async() => {
+  console.log("listening on 4002");
+});
