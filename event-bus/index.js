874bcfb1901600a3685ab6e17c404bf1b327d44c
knmohamed <khalid.n.mohamed@outlook.com>
2024-05-06 20:49:06 -0400

    Add comment moderation and get events ednpoint

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,6 +5,8 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = []
+
 app.post("/events", async (req, res) => {
   const event = req.body;
   axios.post("http://localhost:4000/events", event).catch((err) => {
@@ -16,7 +18,14 @@ app.post("/events", async (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
+  axios.post("http://localhost:4004/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
+});
+
+app.get('/events', (req,res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -46,4 +47,9 @@ app.post("/events", (req, res) => {
 
 app.listen(4002, async () => {
   console.log("listening on 4002");
+  const { data } = await axios.get("http://event-bus-srv:4005/events");
+  for (let event of data) {
+    console.log("Processing event:", event.type);
+    handleEvent(event.type, event.data);
+  }
 });
