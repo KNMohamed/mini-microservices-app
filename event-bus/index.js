750d0dcdb55db0519beac84ef340af1ec850d60c
knmohamed <khalid.n.mohamed@outlook.com>
2024-05-06 20:49:56 -0400

    correct port

--- event-bus/index.js
+++ event-bus/index.js
@@ -18,7 +18,7 @@ app.post("/events", async (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4004/events", event).catch((err) => {
+  axios.post("http://localhost:4003/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
