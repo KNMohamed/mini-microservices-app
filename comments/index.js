132d727f8d7e38c29b7112bf343ef9f41ea8496a
knmohamed <khalid.n.mohamed@outlook.com>
2024-05-06 20:44:40 -0400

    format files

--- comments/index.js
+++ comments/index.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const commentsByPostId = {};
 
-app.post('/posts/:id/comments', async(req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { content } = req.body;
-    const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id, content });
-    commentsByPostId[req.params.id] = comments;
-    await axios.post('http://localhost:4005/events', {
-      type: 'CommentCreated',
-      data: {
-        id,
-        content,
-        postId: req.params.id
-      }
-    });
-    res.status(201).send(comments[comments.length - 1]);
+app.post("/posts/:id/comments", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id, content });
+  commentsByPostId[req.params.id] = comments;
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id,
+      content,
+      postId: req.params.id,
+    },
+  });
+  res.status(201).send(comments[comments.length - 1]);
 });
 
-app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post("/events", (req, res) => {
@@ -37,5 +37,5 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4001, () => {
-  console.log('Listening on 4000');
+  console.log("Listening on 4000");
 });
--- query/index.js
+++ query/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
-const handleEvent = (type,data) => {
-  switch(type){
+const handleEvent = (type, data) => {
+  switch (type) {
     case "PostCreated": {
-      const {id,title} = data;
-      posts[id] = {id,title, comments: []};
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
       break;
     }
     case "CommentCreated": {
-      const {id ,postId, content, status} = data;
-      posts[postId].comments.push({id,content, status});
+      const { id, postId, content, status } = data;
+      posts[postId].comments.push({ id, content, status });
       break;
     }
     case "CommentUpdated": {
-      const {id,content,postId, status} = data;
+      const { id, content, postId, status } = data;
       const post = posts[postId];
-      const comment = post.comments.find(comment => {
+      const comment = post.comments.find((comment) => {
         return comment.id === id;
       });
       comment.content = content;
@@ -31,19 +31,19 @@ const handleEvent = (type,data) => {
       break;
     }
   }
-}
+};
 
-app.get('/posts', (req,res)=>{
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post('/events', (req,res)=>{
-  const {type, data} = req.body;
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
   handleEvent(type, data);
   console.log("Recieved event: ", req.body.type);
-  res.send({status: "OK"});
+  res.send({ status: "OK" });
 });
 
-app.listen(4002, async() => {
+app.listen(4002, async () => {
   console.log("listening on 4002");
 });
