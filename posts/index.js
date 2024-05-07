02e6f9822748d0c5380765c8d21f7a245dce28d9
knmohamed <khalid.n.mohamed@outlook.com>
2024-05-06 20:43:24 -0400

    add prettier

--- posts/index.js
+++ posts/index.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const posts = {};
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', async(req, res) => {
-  const id = randomBytes(4).toString('hex');
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
   posts[id] = {
     id,
-    title
+    title,
   };
 
-  await axios.post('http://localhost:4005/events', {
-    type: 'PostCreated',
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
     data: {
       id,
-      title
-    }
+      title,
+    },
   });
   res.status(201).send(posts[id]);
 });
@@ -39,5 +39,5 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('Listening on 4000');
+  console.log("Listening on 4000");
 });
